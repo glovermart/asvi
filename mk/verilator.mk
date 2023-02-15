f6587e2c58c8ea9cea4af8bfd873852b25b854de
damc <dave.mcewan@nordicsemi.no>
2023-02-15 16:17:53 +0100

    Typo in redirect of STDERR to same place as STDOUT.

--- mk/verilator.mk
+++ mk/verilator.mk
@@ -9,4 +9,4 @@ lint: $(addprefix ${OUTDIR}/,$(addsuffix .verilator_lint.txt,${TESTCASE_NAMES}))
 ${OUTDIR}/%.verilator_lint.txt: testcases/%.sv
 	mkdir -p ${OUTDIR}
 	verilator --version > $@
-	verilator --lint-only $^ >> $@ &2>1
+	verilator --lint-only $^ >> $@ 2>&1
