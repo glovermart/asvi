f1c4998c6ddd875cf3078eadac975cb917d9b011
damc <dave.mcewan@nordicsemi.no>
2023-02-15 14:33:33 +0100

    Add first testcases with template.

--- testcases/alwayscomb_port.sv
+++ testcases/alwayscomb_port.sv
@@ -1,1 +1,28 @@
-TODO
+// Assignment via `always_comb` to scalar members of an SVI port.
+
+interface I;
+
+  logic z;
+  logic y;
+  logic x;
+
+  modport P
+    ( input  z
+    , output y
+    , inout  x
+    );
+
+endinterface
+
+module M
+  ( I.P
+  );
+
+  localparam bit Z = 1'b0;
+  logic a;
+
+  always_comb I.z = Z;      // Constant
+  always_comb I.y = 1'b1;   // Literal
+  always_comb I.x = a;      // Signal
+
+endmodule
--- testcases/alwaysff_inst.sv
+++ testcases/alwaysff_inst.sv
@@ -1,1 +1,59 @@
-TODO
+// Assignment via `always_ff` to scalar members of an SVI instance.
+
+interface I;
+
+  logic z;
+  logic y;
+  logic x;
+
+endinterface
+
+module M
+  ( input var logic i_clk
+  , input var logic i_srst
+  , input var logic i_arst
+  );
+
+  localparam bit Z = 1'b0;
+  logic a;
+
+  I u_I1(); // No reset.
+  always_ff @(posedge i_clk) u_I1.z <= Z;      // Constant
+  always_ff @(posedge i_clk) u_I1.y <= 1'b1;   // Literal
+  always_ff @(posedge i_clk) u_I1.x <= a;      // Signal
+
+  I u_I2(); // Synchronous reset.
+  always_ff @(posedge i_clk)
+    if (i_srst)
+      u_I2.z <= 1'b0;
+    else
+      u_I2.z <= Z;      // Constant
+  always_ff @(posedge i_clk)
+    if (i_srst)
+      u_I2.y <= 1'b0;
+    else
+      u_I2.y <= 1'b1;   // Literal
+  always_ff @(posedge i_clk)
+    if (i_srst)
+      u_I2.x <= 1'b0;
+    else
+      u_I2.x <= a;      // Signal
+
+  I u_I3(); // Asynchronous reset.
+  always_ff @(posedge i_clk, posedge i_arst)
+    if (i_arst)
+      u_I3.z <= 1'b0;
+    else
+      u_I3.z <= Z;      // Constant
+  always_ff @(posedge i_clk, posedge i_arst)
+    if (i_arst)
+      u_I3.y <= 1'b0;
+    else
+      u_I3.y <= 1'b1;   // Literal
+  always_ff @(posedge i_clk, posedge i_arst)
+    if (i_arst)
+      u_I3.x <= 1'b0;
+    else
+      u_I3.x <= a;      // Signal
+
+endmodule
