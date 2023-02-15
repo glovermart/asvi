50cacc6aa395be0bddc2f70e46f1b75ef447d7b0
glovermart <63477001+glovermart@users.noreply.github.com>
2023-02-15 18:21:13 +0100

    replaced tabs with spaces

--- testcases/test1.sv
+++ testcases/test1.sv
@@ -1,29 +1,28 @@
 // Add short description of what's being tested
 
-
 interface in_adders (input int b,c);
-
 
 endinterface
 
 module add_mult(
-    output int a,
-    input int b,c
+ output int a,
+ input int b,c
 );
-    in_adders aci (
-        .b(b),
-        .c(c)
-    );
 
-    // declare internal signals
-    logic [31:0] x;
-    logic [31:0] y;
+in_adders aci (
+ .b(b),
+ .c(c)
+);
 
-    always_comb begin
-    x = aci.b + aci.c;
-    y = aci.b * aci.c;
-    end
+// declare internal signals
+logic [31:0] x;
+logic [31:0] y;
 
-    assign a = x + y;
+always_comb begin
+ x = aci.b + aci.c;
+ y = aci.b * aci.c;
+end
+
+assign a = x + y;
 
 endmodule
