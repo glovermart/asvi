c8204b9e8a68fd7ddd92b0e16bbd940072e1e503
damc <dave.mcewan@nordicsemi.no>
2023-02-15 16:14:01 +0100

    alwayscomb_port Fix syntax for Verilator.

--- testcases/alwayscomb_port.sv
+++ testcases/alwayscomb_port.sv
@@ -7,22 +7,29 @@ interface I;
   logic x;
 
   modport P
-    ( input  z
+    ( output z
     , output y
-    , inout  x
+    , output x
     );
 
 endinterface
 
 module M
-  ( I.P
+  ( I.P p
   );
 
   localparam bit Z = 1'b0;
   logic a;
 
-  always_comb I.z = Z;      // Constant
-  always_comb I.y = 1'b1;   // Literal
-  always_comb I.x = a;      // Signal
+  always_comb p.z = Z;      // Constant
+  always_comb p.y = 1'b1;   // Literal
+  always_comb p.x = a;      // Signal
 
+endmodule
+
+module top;
+  I u_I();
+  M u_M
+    ( .p(u_I)
+    );
 endmodule
