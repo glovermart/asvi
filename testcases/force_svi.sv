1ce5ed7a9623822a64ca3fb41acc8f65b24395ad
glovermart <63477001+glovermart@users.noreply.github.com>
2023-02-21 15:32:14 +0100

    force_svi.sv testcase not tested

--- testcases/force_svi.sv
+++ testcases/force_svi.sv
@@ -1,1 +1,19 @@
-//TODO
+// Assignment via `force` to scalar members within an SVI.
+interface I;
+
+ logic z;
+ logic y;
+ logic x;
+
+ force z = x & y;
+
+endinterface
+
+
+module M;
+
+  I u_I();
+ assign u_I.z = u_I.x | u_I.y;
+ release u_I.z;
+
+endmodule
