39658faeab55dfda34233bb816a135a9fbb2ea44
glovermart <63477001+glovermart@users.noreply.github.com>
2023-02-23 19:18:51 +0100

    fixed syntax error- working now - only first .sv file was being read initially

--- mk/yosys.mk
+++ mk/yosys.mk
@@ -8,5 +8,5 @@ lint: $(addprefix ${OUTDIR}/,$(addsuffix .yosys_lint.txt,${TESTCASE_NAMES}))
 
 ${OUTDIR}/%.yosys_lint.txt: testcases/%.sv
 	mkdir -p ${OUTDIR}
-	yosys -q
+	yosys -q \-p 'read_verilog -sv $^' >> $@ 2>&1
 
