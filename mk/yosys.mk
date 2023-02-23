2884e78d55e24b39856cfc389e45371123430cf3
glovermart <63477001+glovermart@users.noreply.github.com>
2023-02-23 18:11:44 +0100

    fixed error message after testing yosys.mk

--- mk/yosys.mk
+++ mk/yosys.mk
@@ -1,9 +1,11 @@
+
 include mk/common.mk
 
 default: lint
 
 .PHONY: lint
 lint: $(addprefix ${OUTDIR}/,$(addsuffix .yosys_lint.txt,${TESTCASE_NAMES}))
-${OUTDIR}/%/yosys_lint.txt: testcases/%.sv
+
+${OUTDIR}/%.yosys_lint.txt: testcases/%.sv
 	mkdir -p ${OUTDIR}
-	yosys -p read_verilog -sv $^ >> $@ 2>&1
+	yosys
