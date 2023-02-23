3952dc91e0602ee07a1fb4ef7687fba748c3fd6c
glovermart <63477001+glovermart@users.noreply.github.com>
2023-02-23 17:25:57 +0100

    non-tested yosys make

--- mk/yosys.mk
+++ mk/yosys.mk
@@ -1,1 +1,9 @@
-TODO
+include mk/common.mk
+
+default: lint
+
+.PHONY: lint
+lint: $(addprefix ${OUTDIR}/,$(addsuffix .yosys_lint.txt,${TESTCASE_NAMES}))
+${OUTDIR}/%/yosys_lint.txt: testcases/%.sv
+	mkdir -p ${OUTDIR}
+	yosys -p read_verilog -sv $^ >> $@ 2>&1
