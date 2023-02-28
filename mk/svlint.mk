9ba849cf04a5a72c83006c6e3613d22392861220
glovermart <63477001+glovermart@users.noreply.github.com>
2023-02-28 14:21:11 +0100

    svlint test using list of files

--- mk/svlint.mk
+++ mk/svlint.mk
@@ -9,3 +9,6 @@ lint: $(addprefix ${OUTDIR}/,$(addsuffix .svlint_lint.txt,${TESTCASE_NAMES}))
 ${OUTDIR}/%.svlint_lint.txt: testcases/%.sv
 	mkdir -p ${OUTDIR}
 	svlint --version > $@
+	svlint $(TESTCASES)
+
+
