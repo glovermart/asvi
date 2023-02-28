70d1030f7feef1d3cfd6fa1e60b52b83be355628
glovermart <63477001+glovermart@users.noreply.github.com>
2023-02-28 18:11:10 +0100

    writing stderr to stdout - all stdout messages now in first test case's txt file;to fix

--- mk/svlint.mk
+++ mk/svlint.mk
@@ -6,9 +6,9 @@ default: lint
 .PHONY: lint
 lint: $(addprefix ${OUTDIR}/,$(addsuffix .svlint_lint.txt,${TESTCASE_NAMES}))
 
-${OUTDIR}/%.svlint_lint.txt: testcases/%.sv
+${OUTDIR}/%.svlint_lint.txt: testcases/*.sv
 	mkdir -p ${OUTDIR}
 	svlint --version > $@
-	svlint -- --lint $^ >> $@ 2>&1 #To fix - only first file is linted
+	svlint $^ >> $@ 2>&1 #To fix - only first file is linted
 
 
