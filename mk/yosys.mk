6628b536f871315284711f2cd9a6b04037c26967
glovermart <63477001+glovermart@users.noreply.github.com>
2023-02-28 19:13:22 +0100

    fixed exiting on error issue with || true

--- mk/yosys.mk
+++ mk/yosys.mk
@@ -8,5 +8,5 @@ lint: $(addprefix ${OUTDIR}/,$(addsuffix .yosys_lint.txt,${TESTCASE_NAMES}))
 
 ${OUTDIR}/%.yosys_lint.txt: testcases/%.sv
 	mkdir -p ${OUTDIR}
-	yosys -q \-p 'read_verilog -sv $^' >> $@ 2>&1
+	yosys -q \-p 'read_verilog -sv $^' >> $@ 2>&1 || true
 
