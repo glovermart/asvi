25dc4be95c67c5578e5de68047ed05a698b19c57
glovermart <63477001+glovermart@users.noreply.github.com>
2023-02-28 13:24:40 +0100

    make file for svlint start

--- mk/svlint.mk
+++ mk/svlint.mk
@@ -1,1 +1,11 @@
-TODO
+
+include mk/common.mk
+
+default: lint
+
+.PHONY: lint
+lint: $(addprefix ${OUTDIR}/,$(addsuffix .svlint_lint.txt,${TESTCASE_NAMES}))
+
+${OUTDIR}/%.svlint_lint.txt: testcases/%.sv
+ mkdir -p ${OUTDIR}
+	svlint --version > $@
