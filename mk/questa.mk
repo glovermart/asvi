9e8d233e1b383c20fb0a5371e2d9e5efbf0d9a47
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-09 10:27:50 +0100

    initial work -questa

--- mk/questa.mk
+++ mk/questa.mk
@@ -1,1 +1,10 @@
-TODO
+include mk/common.mk
+
+default: lint
+
+.PHONY: lint
+lint: $(addprefix ${OUTDIR}/,$(addsuffix .questa_linter.txt,${TESTCASE_NAMES}))
+
+${OUTDIR}/%.questa_linter.txt: testcases/%.sv
+	mkdir -p ${OUTDIR}
+	vlog -sv -lint #remove -sv?
