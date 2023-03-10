6040e0719c6abe71462de3d8b247a90eefed1ad6
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-11 00:57:06 +0100

    enabled sfcu for compiling each file separately but not working

--- tcl/batch_run.tcl
+++ tcl/batch_run.tcl
@@ -1,6 +1,7 @@
 new_project spyG_lint -projectwdir spyglass -force
 set_option enableSV yes
-read_file testcases/test1.sv
+set_option sfcu yes
+read_file {testcases/*.sv}
 current_goal lint/lint_rtl -alltop
 run_goal
 capture out/spyglass_lint.rpt {write_report moresimple}
