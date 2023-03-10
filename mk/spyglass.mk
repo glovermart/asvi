d2e6afbe9e864b538fd272ed8614a2eea3c5bd3c
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-10 22:57:15 +0100

    expliicitly create project with a aname and specify working directory; wdir = out/

--- mk/spyglass.mk
+++ mk/spyglass.mk
@@ -1,5 +1,6 @@
 include mk/common.mk
 
 default: lint
+	mkdir -p ${OUTDIR}
 	spyglass -shell -tcl tcl/batch_run.tcl
 .PHONY: lint
--- tcl/batch_run.tcl
+++ tcl/batch_run.tcl
@@ -1,5 +1,6 @@
+new_project spyG_lint -projectwdir out -force
 set_option enableSV yes
-read_file testcases/test1.sv
+read_file testcases/*.sv
 current_goal lint/lint_rtl -alltop
 run_goal
 exit -force
