cf7783de3deb5c2247927008df77a387dae484b2
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-10 23:33:32 +0100

    capture lint & read report (moresimple.rpt) into out directory

--- mk/spyglass.mk
+++ mk/spyglass.mk
@@ -2,5 +2,6 @@ include mk/common.mk
 
 default: lint
 	mkdir -p ${SPYWkDIR}
+	mkdir -p ${OUTDIR}
 	spyglass -shell -tcl tcl/batch_run.tcl
 .PHONY: lint
--- tcl/batch_run.tcl
+++ tcl/batch_run.tcl
@@ -3,4 +3,5 @@ set_option enableSV yes
 read_file testcases/test1.sv
 current_goal lint/lint_rtl -alltop
 run_goal
+capture out/spyglass_lint.rpt {write_report moresimple}
 exit -force
