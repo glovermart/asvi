ad49100fb4f636d8a5a4300ab5f91792e2f78cf3
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-14 15:46:41 +0100

    formality - included variable from perequisite - automate reading of testcases

--- mk/formality.mk
+++ mk/formality.mk
@@ -8,4 +8,4 @@ read: $(addprefix ${OUTDIR}/,$(addsuffix .formality_read.txt,${TESTCASE_NAMES}))
 ${OUTDIR}/%.formality_read.txt: testcases/%.sv
 	mkdir -p ${OUTDIR}
 	fm_shell -version >> $@
-	fm_shell -work_path out/formality -x "read_sverilog -r testcases/test1.sv;quit"
+	fm_shell -work_path out/formality -x "read_sverilog -r $^;quit"
