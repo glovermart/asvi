69700aff0ae04c4b961168bfbfd550d1091967a4
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-14 16:16:31 +0100

    removed displaying of formality version before the read recipe

--- mk/formality.mk
+++ mk/formality.mk
@@ -7,5 +7,4 @@ read: $(addprefix ${OUTDIR}/,$(addsuffix .formality_read.log,${TESTCASE_NAMES}))
 
 ${OUTDIR}/%.formality_read.log: testcases/%.sv
 	mkdir -p ${OUTDIR}
-	fm_shell -version >> $@
 	fm_shell -work_path out/formality -x "read_sverilog -r $^;quit" >> $@ 2>&1
