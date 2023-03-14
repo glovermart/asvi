530159f678947faa262dc220fb89d917af314e3d
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-14 22:10:52 +0100

    fixed 'cannot add input file' error - inserted absolute path to file

--- tcl/pcn_batch_run.tcl
+++ tcl/pcn_batch_run.tcl
@@ -1,6 +1,6 @@
-new_project -name "precision_runs" -folder "out" -createimpl -createimpl_name prn_run -force
+new_project -name "precision_runs" -folder "out/" -createimpl -createimpl_name prn_run -force
 setup_design -hdl SystemVerilog2012
-add_input_file testcases/test1.sv
+add_input_file <insert abolute path here>
 remove_design -designs
 remove_input_file -all
 exit -force
