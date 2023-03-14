4d8acb23c050c6dd4948d34013fcc3b9a7386da8
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-14 21:07:57 +0100

    precision - specifiy language standard/format using setup_design command

--- tcl/pcn_batch_run.tcl
+++ tcl/pcn_batch_run.tcl
@@ -1,3 +1,5 @@
-add_input_file -format SystemVerilog2012 testcases/test1.sv -work out
+setup_design -hdl SystemVerilog2012
+add_input_file testcases/test1.sv
 remove_design -designs
+remove_input_file -all
 exit -force
