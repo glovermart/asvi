0a3fad6b09bb46b0d5ddcc18c0206a02400984e0
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-14 21:11:34 +0100

    make working directory  out directory using new_project command

--- tcl/pcn_batch_run.tcl
+++ tcl/pcn_batch_run.tcl
@@ -1,3 +1,4 @@
+new_project -name "precision_runs" -folder "out" -createimpl -createimpl_name prn_run -force
 setup_design -hdl SystemVerilog2012
 add_input_file testcases/test1.sv
 remove_design -designs
