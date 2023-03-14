3a44869ec9990394d60a0f3628579c2fc0dd9315
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-14 15:36:51 +0100

    formality - changed to command string format -x

--- mk/formality.mk
+++ mk/formality.mk
@@ -1,11 +1,11 @@
 include mk/common.mk
 
-default: formal
+default: read
 
-.PHONY: formal
-formal: $(addprefix ${OUTDIR}/,$(addsuffix .formal_formality.txt,${TESTCASE_NAMES}))
+.PHONY: read
+read: $(addprefix ${OUTDIR}/,$(addsuffix .formality_read.txt,${TESTCASE_NAMES}))
 
-${OUTDIR}/%.formal_formality.txt: testcases/%.sv
+${OUTDIR}/%.formality_read.txt: testcases/%.sv
 	mkdir -p ${OUTDIR}
 	fm_shell -version >> $@
-	fm_shell -work_path out/formality -file tcl/fm_batch_run.tcl
+	fm_shell -work_path out/formality -x "read_sverilog -r testcases/test1.sv;quit"
