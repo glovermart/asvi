8c3e72c41f9605b2b8a62e6174d3dd0f0163da07
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-14 16:07:35 +0100

    stderr to .log files - to do; keep error or warning messages only

--- mk/formality.mk
+++ mk/formality.mk
@@ -3,9 +3,9 @@ include mk/common.mk
 default: read
 
 .PHONY: read
-read: $(addprefix ${OUTDIR}/,$(addsuffix .formality_read.txt,${TESTCASE_NAMES}))
+read: $(addprefix ${OUTDIR}/,$(addsuffix .formality_read.log,${TESTCASE_NAMES}))
 
-${OUTDIR}/%.formality_read.txt: testcases/%.sv
+${OUTDIR}/%.formality_read.log: testcases/%.sv
 	mkdir -p ${OUTDIR}
 	fm_shell -version >> $@
 	fm_shell -work_path out/formality -x "read_sverilog -r $^;quit" >> $@ 2>&1
