9975c454dcc0eb6226a967af8fcb4f21b89f8558
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-15 19:33:39 +0100

    fixed absolute path requred for add_input_file command , set path placed before new_project command

--- tcl/pcn_batch_run.tcl
+++ tcl/pcn_batch_run.tcl
@@ -1,6 +1,6 @@
+set path [pwd]
 new_project -name "precision_runs" -folder "out/" -createimpl -createimpl_name prn_run -force
 setup_design -hdl SystemVerilog2012
-set path [pwd]
 add_input_file $path/testcases/force_svi.sv
 remove_design -designs
 remove_input_file -all
