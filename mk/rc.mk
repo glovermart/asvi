2b484a4acad1acdb4c70997cf7d4309ea06c4907
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-15 19:23:13 +0100

    fixed syntax error in target/prerequisite

--- mk/rc.mk
+++ mk/rc.mk
@@ -3,7 +3,7 @@ include mk/common.mk
 default: read
 
 .PHONY: read
-read: $(addprefix ${OUTDIR}/,$(addsuffix .rcread.txt,${TESTCASE_NAMES}))
+read: $(addprefix ${OUTDIR}/,$(addsuffix .rc_read.txt,${TESTCASE_NAMES}))
 
 ${OUTDIR}/%.rc_read.txt: testcases/%.sv
 	mkdir -p ${OUTDIR}
