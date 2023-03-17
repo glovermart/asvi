bee814338446f9d804ac1879752b8d32eea97696
glovermart <63477001+glovermart@users.noreply.github.com>
2023-03-17 20:40:49 +0100

    write to output files; continue on error

--- mk/precision.mk
+++ mk/precision.mk
@@ -7,4 +7,4 @@ read: $(addprefix ${OUTDIR}/,$(addsuffix .precision_read.log,${TESTCASE_NAMES}))
 
 ${OUTDIR}/%.precision_read.log: testcases/%.sv
 	mkdir -p ${OUTDIR}
-	precision -shell -logfile out/precision.log -file tcl/pcn_batch_run.tcl -fileargs $^
+	-precision -shell -logfile out/precision.log -file tcl/pcn_batch_run.tcl -fileargs $^ >> $@ 2>&1
