aa5092a81e657d0e3b48ad6d8e04058e9207c3e1
glovermart <63477001+glovermart@users.noreply.github.com>
2023-04-05 21:09:48 +0200

    Added reason for selecting the 2012 sv standard

--- mk/icarus.mk
+++ mk/icarus.mk
@@ -6,7 +6,7 @@ default: lint
 lint: $(addprefix ${OUTDIR}/,$(addsuffix .icarus_lint.stdout,${TESTCASE_NAMES}))
 
 # -tnull: No specific target for compilation; no code generation.
-# -g2012: Select the IEEE1800-2012 standard.
+# -g2012: Select the IEEE1800-2012 standard. Note: No support yet for the IEEE1800-2017 standard.
 # -Wall: Display all warnings.
 # -i: Ignore missing modules.
 # https://www.mankier.com/1/iverilog
