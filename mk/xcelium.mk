8c1cbcf8bc6dbf41ce8ca8ad592f4da215eb89eb
glovermart <63477001+glovermart@users.noreply.github.com>
2023-04-05 20:11:11 +0200

    Changed output file extension to .stdout. Details addeed; flag comments and command references.

--- mk/xcelium.mk
+++ mk/xcelium.mk
@@ -1,13 +1,29 @@
+# Load Cadence Xcelium tool
+# module load cadence/xcelium/22.03.004
+
 include mk/common.mk
 
 default: simulate
 
 .PHONY: simulate
-simulate: $(addprefix ${OUTDIR}/,$(addsuffix .xcelium_simulate.txt,${TESTCASE_NAMES}))
+simulate: $(addprefix ${OUTDIR}/,$(addsuffix \
+.xcelium_simulate.stdout,${TESTCASE_NAMES}))
+
+# See Cadence Xcelium Overview and syntax html files.
+#  .../vlogcompopts/Overview_and_Syntax.html
+#  .../ElaboratorOptions/Overview_and_Syntax.html
+#  .../SimulatorOptions/Overview_and_Syntax.html
+#  .../xsctutorial/Preparing_Your_Design_for_Simulation.html
+# -sv: Enable SystemVerilog language constructs.
+# -nocopyright: Do not print copyright banner.
+# -nolog: Do not generate a logfile.
 
 XCELIUM_TOP ?= tb
-${OUTDIR}/%.xcelium_simulate.txt: testcases/%.sv testbench/tb.sv
-	mkdir -p ${OUTDIR}
-	-xmvlog -sv -nocopyright -nolog $^ >> $@ 2>&1
-	-xmelab -nocopyright -nolog ${XCELIUM_TOP} >> $@ 2>&1
-	-xmsim -nocopyright -nolog ${XCELIUM_TOP} >> $@ 2>&1
+${OUTDIR}/%.xcelium_read.stdout: testcases/%.sv testbench/tb.sv | ${OUTDIR}
+	-xmvlog -sv -nocopyright -nolog $^ ${REDIRECT}
+
+${OUTDIR}/%.xcelium_elaborate.stdout: ${OUTDIR}/%.xcelium_read.stdout
+	-xmelab -nocopyright -nolog ${XCELIUM_TOP} ${REDIRECT}
+
+${OUTDIR}/%.xcelium_simulate.stdout: ${OUTDIR}/%.xcelium_elaborate.stdout
+	-xmsim -nocopyright -nolog ${XCELIUM_TOP} ${REDIRECT}
