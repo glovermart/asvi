a1e44c6252fe2a25e4e1ffae94c45102cbdfad24
glovermart <63477001+glovermart@users.noreply.github.com>
2023-04-12 17:00:12 +0200

    Removed pdf numbering and changed '&' to 'and'

--- mk/verilator.mk
+++ mk/verilator.mk
@@ -6,10 +6,10 @@ default: lint
 lint: $(addprefix ${OUTDIR}/,$(addsuffix .verilator_lint.stdout,${TESTCASE_NAMES}))
 
 # See Verilator maunual, Release Devel 5.009, Wilson Snyder 2023-03-18
-# --lint-only: Lint the design - check for warnings & errors; no output file.
-#  Pages 16(19) & 77(80).
+# --lint-only: Lint the design - check for warnings and errors; no output file.
+#  Pages 16 and 77.
 # -Wall: Enable all warning and writing style messages (enables stronger linting).
-#  Pages 85(88), 2(5) & 68(71).
+#  Pages 85, 2 and 68.
 
 ${OUTDIR}/%.verilator_lint.stdout: testcases/%.sv | ${OUTDIR}
 	-verilator --lint-only -Wall $^ ${REDIRECT}
