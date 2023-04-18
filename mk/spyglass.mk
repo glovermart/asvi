39077ba999df5bfd6bb3ab3640f7d0e108ec8167
glovermart <63477001+glovermart@users.noreply.github.com>
2023-04-18 17:35:13 +0200

    Changed output file extension to .stdout. Details addeed; flag comments and command references.

--- mk/spyglass.mk
+++ mk/spyglass.mk
@@ -1,12 +1,28 @@
+#Load Synopsys Spyglass
+#module load synopsys/spyglass/2023.03
+
 include mk/common.mk
 SPYGLASS_WORK_DIR := spyglass
 default: lint
 
 .PHONY: lint
-lint: $(addprefix ${OUTDIR}/,$(addsuffix .spyglass_lint.txt,${TESTCASE_NAMES}))
+lint: $(addprefix ${OUTDIR}/,\
+	$(addsuffix .spyglass_lint.stdout,${TESTCASE_NAMES}))
+
 export TESTCASE = $^
 export TESTCASE_REPORT = $@
-${OUTDIR}/%.spyglass_lint.txt: testcases/%.sv
+
+# See Spyglass Tcl Shell Interface User Guide, Version U-2023.03
+# SpyGlass_TclShellInterface_UserGuide.pdf
+# pages 39, 40, 42
+#
+# For commands used in tcl script,
+# see pages 92, 93, 129, 169, 216, 889, 890, 316, 317, 108, 109 
+#
+# For argument enableSV12 used with the set_option command in tcl script,
+# see SpyGlass Explorer User Guide, Version U-2023.03
+# SpyGlass_Explorer_UserGuide.pdf page 852
+#
+${OUTDIR}/%.spyglass_lint.stdout: testcases/%.sv | ${OUTDIR}
 	mkdir -p ${SPYGLASS_WORK_DIR}
-	mkdir -p ${OUTDIR}
-	sg_shell < tcl/spyglass_batch_run.tcl
+	sg_shell < tcl/spyglass_batch_run.tcl ${REDIRECT}
