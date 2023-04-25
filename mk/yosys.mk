6c989c29232ef8f699c68576ff8d5b3bed910458
glovermart <63477001+glovermart@users.noreply.github.com>
2023-04-25 08:22:37 +0200

    Write synthesized netlists
    - Replaced opt and proc commands with generic synthesis command synth.
    - Added command to write the snthesized netlist.

--- mk/yosys.mk
+++ mk/yosys.mk
@@ -6,10 +6,13 @@ default: read
 
 read: $(addprefix ${OUTDIR}/,$(addsuffix .yosys_read.stdout,${TESTCASE_NAMES}))
 # See Yosys Manual, Version 2023
-#  Pages 157, 281, 89
+#  Pages 157, 281, 89, 232, 207, and 313
 # -q: Suppress yosys invocation message on start-up; do not print output.
 # -p: Specify yosys commands on the command line.
 # -sv: Enable support for SystemVeerilog features.
+# -top: Specify top module.
+#
+# https://yosyshq.net/yosys/about.html
 
 ${OUTDIR}/%.yosys_read.stdout: testcases/%.sv | ${OUTDIR}
-	-yosys -q -p "read_verilog -sv $^; proc; opt" ${REDIRECT}
+	-yosys -q -p "read_verilog -sv $^; synth -top top; write_verilog $@_synth.v" ${REDIRECT}
