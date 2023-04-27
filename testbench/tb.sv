1e5eb9c5e3820cb072fb8bf5655ad9b3269f8f12
glovermart <63477001+glovermart@users.noreply.github.com>
2023-04-27 22:02:26 +0200

    testbench for alwayscomb_arraySVI_inst.sv and alwayscomb_arraySVI_port.sv

--- testbench/tb.sv
+++ testbench/tb.sv
@@ -1,8 +1,16 @@
 `default_nettype none
-module tb;
-  top u_DUT();
+module tb
+    ( output logic [7:0] o_a
+    , output logic [7:0] o_b
+    , output logic [7:0] o_c
+    );
+  top u_DUT(.o_a,.o_b,.o_c);
   initial begin: l_tb_initial
     $info("Simulation started.");
+    #100
+    $display("value of o_a is %0b",o_a);
+    $display("value of o_a is %0b",o_b);
+    $display("value of o_a is %0b",o_c);
     $finish;
   end: l_tb_initial
 endmodule
