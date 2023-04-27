09ee8d48cf5d5bf8527983f2c11b1662206e0547
glovermart <63477001+glovermart@users.noreply.github.com>
2023-04-27 21:51:57 +0200

    clean and extended macro for arrays

--- testcases/alwayscomb_arraySVI_inst.sv
+++ testcases/alwayscomb_arraySVI_inst.sv
@@ -10,27 +10,37 @@ interface I;
 endinterface
 
 module M
-  ( I u_I1[`duplicate-1:0]
+  ( I u_I[`duplicate-1:0]
   );
 
   localparam bit Z = 1'b0;
   logic a;
 
   for (genvar i=0; i<`duplicate; i++) begin
-    always_comb u_I1[i].x = Z;      // Constant
-    always_comb u_I1[i].y = 1'b1;   // Literal
-    always_comb u_I1[i].z = a;      // Signal
+    always_comb u_I[i].x = Z;      // Constant
+    always_comb u_I[i].y = 1'b1;   // Literal
+    always_comb u_I[i].z = a;      // Signal
   end
 
 endmodule
 
-module top(input logic clk, output logic [7:0] o_a, o_b, o_c);
-  I u_I1[`duplicate-1:0] ();
-  M u_M (.u_I1(u_I1));
+module top
+  ( input logic clk 
+  , output logic [`duplicate-1:0] o_a
+  , output logic [`duplicate-1:0] o_b
+  , output logic [`duplicate-1:0] o_c
+  );
   
+  I u_I[`duplicate-1:0] ();
+  
+  M u_M 
+    ( .*
+    );
+
   for (genvar i=0;i<`duplicate;i++)begin
-    assign o_a[i] = u_I1[i].x;      
-    assign o_b[i] = u_I1[i].y;  
-    assign o_c[i] = u_I1[i].z;    
+    assign o_a[i] = u_I[i].x;      
+    assign o_b[i] = u_I[i].y;  
+    assign o_c[i] = u_I[i].z;    
   end
+
 endmodule
