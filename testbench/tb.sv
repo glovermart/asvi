c17d024c04ed99ed87d4915ea628f2ed2934bc62
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-02 19:55:30 +0200

    alwayslatch_arrayofSVI_inst initial work with testbench

--- testbench/tb.sv
+++ testbench/tb.sv
@@ -2,11 +2,10 @@
 module tb;
   timeunit 1ns;
   timeprecision 1ps;
-
   logic i_clk;
-  logic i_a;
-  logic i_b;
-  logic i_c;
+  logic i_srst;
+  logic i_arst;
+  logic en;
   logic [7:0] o_a;
   logic [7:0] o_b;
   logic [7:0] o_c;
@@ -19,10 +18,24 @@ module tb;
   initial begin: l_tb_initial
     $info("Simulation started.");
     i_clk <= 0;
+    en <= 1;
+    i_arst <= 1;
     $display("output values at time %0t",$time);
     $display("value of o_a is %0b",o_a);
     $display("value of o_b is %0b",o_b);
     $display("value of o_c is %0b",o_c);
+    repeat (9)
+    @ (posedge i_clk);
+    i_arst <= 0;
+    repeat (4)
+    @ (posedge i_clk);
+    i_arst <= 1;
+    repeat (9)
+    @ (posedge i_clk);    
+    i_arst <= 0;
+    repeat (9)
+    @ (posedge i_clk);    
+    en <= 0;
     #sim_end
     $display("output values at time %0t",$time);
     $display("value of o_a is %0b",o_a);
