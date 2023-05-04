d74ff1b6ee744d2895483f34f90bad529b01557f
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-05 00:05:15 +0200

    Changed `define compiler directive to localparam

--- testcases/assign_arrayofSVI_inst.sv
+++ testcases/assign_arrayofSVI_inst.sv
@@ -1,7 +1,8 @@
 // Assignment via `assign` to scalar members of an SVI instance.
 // Array of SVIs
 
-`define duplicate 8
+localparam int SIZE = 8;
+
 interface I;
 
   logic x;
@@ -11,39 +12,39 @@ interface I;
 endinterface
 
 module M
-  ( I u_I[`duplicate-1:0]
+  ( I u_I[SIZE-1:0]
   , input logic i_a   // avoid variable not driven warnings
   );
 
   localparam bit Z = 1'b0;
-  genvar i;
-  generate
-  for (i=0; i<`duplicate; i++) begin   
+  
+  
+  for (genvar i=0; i < SIZE; i++) begin   
     assign u_I[i].x = Z;      // Constant
     assign u_I[i].y = 1'b1;   // Literal
     assign u_I[i].z = i_a;      // Signal
   end
-  endgenerate
+  
 endmodule
 
 module top
   ( input logic i_a
-  , output logic [`duplicate-1:0] o_a
-  , output logic [`duplicate-1:0] o_b
-  , output logic [`duplicate-1:0] o_c
+  , output logic [SIZE-1:0] o_a
+  , output logic [SIZE-1:0] o_b
+  , output logic [SIZE-1:0] o_c
   );
   
-  I u_I[`duplicate-1:0] ();
+  I u_I[SIZE-1:0] ();
   
   M u_M 
-    ( .*
+    ( .u_I
     , .i_a
     );
-
-  for (genvar i=0;i<`duplicate;i++)begin
+  
+  for (genvar i=0;i<SIZE;i++)begin
     assign o_a[i] = u_I[i].x;      
     assign o_b[i] = u_I[i].y;  
     assign o_c[i] = u_I[i].z;    
   end
-
+  
 endmodule
