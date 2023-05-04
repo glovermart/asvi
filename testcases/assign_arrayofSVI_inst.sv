d953e64ca11dc839e9d0ef8df93fd61c0e147a8e
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-04 23:08:27 +0200

    initial work

---
+++ testcases/assign_arrayofSVI_inst.sv
@@ -0,0 +1,46 @@
+// Assignment via `assign` to scalar members of an SVI instance.
+// Array of SVIs
+
+`define duplicate 8
+interface I;
+
+  logic x;
+  logic y;
+  logic z;
+
+endinterface
+
+module M
+  ( I u_I[`duplicate-1:0]
+  );
+
+  localparam bit Z = 1'b0;
+  logic a;
+
+  for (genvar i=0; i<`duplicate; i++) begin  
+    assign u_I[i].x = Z;      // Constant
+    assign u_I[i].y = 1'b1;   // Literal
+    assign u_I[i].z = a;      // Signal
+  end
+
+endmodule
+
+module top
+  ( output logic [`duplicate-1:0] o_a
+  , output logic [`duplicate-1:0] o_b
+  , output logic [`duplicate-1:0] o_c
+  );
+  
+  I u_I[`duplicate-1:0] ();
+  
+  M u_M 
+    ( .*
+    );
+
+  for (genvar i=0;i<`duplicate;i++)begin
+    assign o_a[i] = u_I[i].x;      
+    assign o_b[i] = u_I[i].y;  
+    assign o_c[i] = u_I[i].z;    
+  end
+
+endmodule
