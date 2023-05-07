9e058427e0d6cfdacce47e8c452bd5cd1c1bcafe
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-07 13:02:23 +0200

    revert to initial scenario - simulation now works; output waveform not predictable

--- testcases/alwaysff_packed_port.sv
+++ testcases/alwaysff_packed_port.sv
@@ -1,11 +1,33 @@
 // Assignment via `always_ff` and modport to a packed array within an interface.
-// A case without modport used.
+// Write to each bit location every rising edge of i_clk.
+// No for loop.
 
 interface I;
 
   logic [7:0] x; //Packed array x
-
+  modport P
+    ( output x
+    );
 endinterface
+
+
+module M
+  ( I.P p 
+  , output logic [7:0] o_a
+  , input logic i_a
+  , input logic i_clk
+  );
+  
+  int i = 0;  
+  always_ff @ (posedge i_clk) 
+    p.x[i] <= i_a;
+  
+  always_ff @ (posedge i_clk)     
+    i <= i + 'd1;          
+  
+  assign o_a = p.x;
+
+endmodule
 
 module top
   ( input logic i_a
@@ -14,13 +36,10 @@ module top
   );
 
   I u_I ();
-  int i = 0;
-  
-  always_ff @ (posedge i_clk)begin 
-    u_I.x[i] <= i_a;
-    i <= i + 'd1; // Single processes with clock enable inferred.
-  end             // Each register is written @ i_clk posedge.
-
-  assign o_a = u_I.x;
-
+  M u_M
+    ( .o_a
+    , .i_a
+    , .i_clk
+    , .p(u_I)
+    );
 endmodule
