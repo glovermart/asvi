0d676ccb0c8ad81fb690a964de0cc2032e30541c
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-07 11:14:05 +0200

    testbench for assign and alwaysff experiments

--- testbench/tb.sv
+++ testbench/tb.sv
@@ -1,17 +1,17 @@
 `default_nettype none
 module tb;
   logic [7:0] o_a;
-  logic clk;
+  logic i_clk;
   logic i_a;
-  always #2 clk = ~ clk;
+  always #2 i_clk = ~ i_clk;
   top u_DUT(.*);
   initial begin: l_tb_initial
     $info("Simulation started.");
-    clk <= 0;
+    i_clk <= 0;
     i_a <= 1;
     $display("The output is %b", o_a);
     repeat (15)
-    @ (posedge clk);
+    @ (posedge i_clk);
     $display("The output is now %b", o_a);
     $finish;
   end: l_tb_initial
