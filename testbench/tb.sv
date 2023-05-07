c98f1540de519d8e4d051d5a9e2d75503bfa9575
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-07 03:32:44 +0200

    testbench for alwayscomb_pack_*

--- testbench/tb.sv
+++ testbench/tb.sv
@@ -1,8 +1,18 @@
 `default_nettype none
 module tb;
-  top u_DUT();
+  logic [7:0] o_a;
+  logic clk;
+  logic i_a;
+  always #2 clk = ~ clk;
+  top u_DUT(.*);
   initial begin: l_tb_initial
     $info("Simulation started.");
+    clk <= 0;
+    i_a <= 1;
+    $display("The output is %b", o_a);
+    repeat (15)
+    @ (posedge clk);
+    $display("The output is now %b", o_a);
     $finish;
   end: l_tb_initial
 endmodule
