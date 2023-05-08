f7e0e3dfcc35cf2ce7b76495793f335e5384181a
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-08 15:03:05 +0200

    initial work for force experiments with testbench

--- testbench/tb.sv
+++ testbench/tb.sv
@@ -3,16 +3,34 @@ module tb;
   logic [7:0] o_a;
   logic i_clk;
   logic i_a;
-  always #2 i_clk = ~ i_clk;
+  logic en;
+  
+  parameter clk_period = 2;
+  parameter sim_end = clk_period*5;
+  always #clk_period i_clk = ~ i_clk;
+  
   top u_DUT(.*);
+  
   initial begin: l_tb_initial
     $info("Simulation started.");
     i_clk <= 0;
-    i_a <= 1;
-    $display("The output is %b", o_a);
-    repeat (15)
+    en <= 1;
+    i_a <= 0;
+    $display("output values at time %0t",$time);
+    $display("value of o_a is %0b",o_a);
     @ (posedge i_clk);
-    $display("The output is now %b", o_a);
+    $display("output values at time %0t",$time);
+    $display("value of o_a is %0b",o_a);   
+    @ (posedge i_clk);
+    en  <= 0;
+    @ (posedge i_clk);    
+    $display("output values at time %0t",$time);
+    $display("value of o_a is %0b",o_a);    
+    @ (posedge i_clk);    
+    i_a <= 1;
+    #sim_end
+    $display("output values at time %0t",$time);
+    $display("value of o_a is %0b",o_a);
     $finish;
   end: l_tb_initial
 endmodule
