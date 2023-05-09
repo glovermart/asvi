00f818a8c3f9db5aeb2d17c953d5fd09e64d29bb
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-09 14:39:52 +0200

    added always_ff event control and non-blocking assignment

--- testcases/alwaysff_unpacked_svi.sv
+++ testcases/alwaysff_unpacked_svi.sv
@@ -1,17 +1,18 @@
 // Assignment via `always_ff` to an unpacked array within an interface.
 // Use `foreach` loop to iterate through array elements.
-// Always_ff without event control; blocking assignment used in always_ff block.
+// Always_ff with event control and non-blocking assignment.
 
 interface I
-  ( input logic i_a
+  ( input logic i_clk
+  , input logic i_a
   , output logic o_a [8] // SV allows C-like declaration of an unpacked array.
   );
 
   logic x [7:0]; // Unpacked array x with starting and ending addresses.
   
-  always_ff
+  always_ff @ (posedge i_clk)
     foreach (x[i])
-      x[i] = i_a;
+      x[i] <= i_a;
    
   assign o_a = x; // Copy array to output pins.
 
@@ -19,13 +20,13 @@ endinterface
 
 
 module top
-  ( input logic i_a
+  ( input logic i_clk
+  , input logic i_a
   , output logic o_a [7:0]
   );
 
   I u_I 
-  ( i_a // Ordered port list connection.
-  , o_a
+  ( .* // Implicit port connection (dot-star).
   );
   
 endmodule
