ccf08428ac3942285abe71ff655757c3aa59609f
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-10 16:00:46 +0200

    LSBs part select; what is expected from modport cases

---
+++ testcases/alwayscomb_unpacked_LSBs_example.sv
@@ -0,0 +1,31 @@
+// Assignment via `always_comb` to an unpacked array within an interface. 
+
+interface I
+  ( input logic i_a
+  , input logic i_en
+  );
+
+  logic x [7:0]; // Unpacked array x.
+  
+  always_comb 
+    if (i_en)
+      x = '{i_a,i_a,i_a,i_a,'z,'z,'z,'z}; // Drive LSBs to 'z',
+    else
+      x = '{8{i_a}}; // Write all array elements with the same value. 
+
+endinterface
+
+module top
+  ( input logic i_a
+  , input logic i_en
+  , output logic o_a [7:0]
+  );
+
+  I u_I 
+    ( .i_a(i_a)   
+    , .i_en(i_en)
+    );
+
+  assign o_a = u_I.x; // Copy values to output.
+
+endmodule
