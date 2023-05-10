ef2431d30d5657df52bd9b7a80111b77ac96afef
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-10 18:10:08 +0200

    introduced another modport expression and changed always_comb to always @

--- testcases/force_unpacked_port.sv
+++ testcases/force_unpacked_port.sv
@@ -1,34 +1,43 @@
 // Assignment via `force` to an unpacked array of an SVI instance.
 // Use modport expression to force the 4 LSBs to high-impedance state (z).
-/* At module instantiation, modport view is removed i.e.
-(.i(u_I) instead of .i(u_I.P)) */
+
 
 interface I;
   
   logic z [7:0];
   
   modport P
-    ( output .p(z[3:0]) // LSBs
+    ( output .p(z[0+:4]) // LSBs. Using addressing style from LRM 11.5.1.
+    , output .q(z[7-:4]) // MSBs. Using addressing style from LRM 11.5.1.
     );
 
 endinterface
 
 module M
   ( interface i  // Generic interface i.
-  , input bit en 
+  , input logic i_en 
+  , input logic i_a
   );
 
-  always_comb
-    if (en)
-      force i.p = '{4{'z}};
+  always @ (i_en)
+ // always_comb behaves the same way. 
+    if (i_en)
+      force i.p = '{4{1'bz}};
     else
       release i.p;
+
+  always @ (i_en)
+ // always_comb behaves the same way.
+    if (i_en)
+      force i.q = '{4{i_a}};
+    else
+      release i.q;
 
 endmodule
 
 module top
-  ( input bit en
-  , input var logic i_a
+  ( input logic i_en
+  , input logic i_a
   , output logic o_a [7:0]
   );
 
@@ -37,8 +46,9 @@ module top
   assign u_I.z = '{8{i_a}}; // Fill the array with some bit of the same value.
 
   M u_M 
-    ( .en(en)
-    , .i(u_I)
+    ( .i_en(i_en)
+    , .i(u_I.P)
+    , .i_a(i_a)
     );
 
   assign o_a = u_I.z; // Copy/output the array elements.
