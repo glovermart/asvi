7314dd20701cd3d0b4a530983499513d4fe862d6
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-17 10:54:53 +0200

    changed text insertion from define to localparam

--- testcases/alwayscomb_arraySVI_inst.sv
+++ testcases/alwayscomb_arraySVI_inst.sv
@@ -1,6 +1,6 @@
 // Assignment via `always_comb` to scalar members of an SVI instance.
 // Array of SVIs
-`define duplicate 8
+localparam int SIZE = 8;
 interface I;
 
   logic x;
@@ -10,13 +10,13 @@ interface I;
 endinterface
 
 module M
-  ( I u_I[`duplicate-1:0]
+  ( I u_I[SIZE-1:0]
   );
 
   localparam bit Z = 1'b0;
   logic a;
 
-  for (genvar i=0; i<`duplicate; i++) begin
+  for (genvar i=0; i<SIZE; i++) begin
     always_comb u_I[i].x = Z;      // Constant
     always_comb u_I[i].y = 1'b1;   // Literal
     always_comb u_I[i].z = a;      // Signal
@@ -26,18 +26,18 @@ endmodule
 
 module top
   ( input logic i_clk 
-  , output logic [`duplicate-1:0] o_a
-  , output logic [`duplicate-1:0] o_b
-  , output logic [`duplicate-1:0] o_c
+  , output logic [SIZE-1:0] o_a
+  , output logic [SIZE-1:0] o_b
+  , output logic [SIZE-1:0] o_c
   );
   
-  I u_I[`duplicate-1:0] ();
+  I u_I[SIZE-1:0] ();
   
   M u_M 
     ( .*
     );
 
-  for (genvar i=0;i<`duplicate;i++)begin
+  for (genvar i=0;i<SIZE;i++)begin
     assign o_a[i] = u_I[i].x;      
     assign o_b[i] = u_I[i].y;  
     assign o_c[i] = u_I[i].z;    
