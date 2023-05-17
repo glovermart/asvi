119322ae6ddf5f50f2d83177afa3c9690e0cfd8b
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-17 13:48:38 +0200

    split experiments

--- testcases/alwayslatch_arrayofSVI_port_1.sv
+++ testcases/alwayslatch_arrayofSVI_port_1.sv
@@ -1,89 +1,74 @@
 // Assignment via `always_latch` to scalar members of an SVI port.
 // Array of SVIs
-
 // Using modports, output 'y' from module M1 is an input to module M2.
+// Modules M1 and M2 now with en and arst pins conected directly to 
+// top module pins.
 
-/* Modules M1 and M2 now with en and arst pins connected directly to 
- top module pins.*/
-
-/* Removed nets not used in modport to avoid inferred connection to 
- to the input or output of other module (M1 or M2).*/
-
-
-`define V 8
+localparam int SIZE = 8;
 
 interface I;
-  timeunit 1ns;
-  timeprecision 1ps;
   
   logic y;
+  logic x;
+  logic w;
 
   modport P1
-    ( 
-     output y
+    ( input x
+    , output y
     );
 
   modport P2
     ( input y
+    , output w
     );
 
 endinterface
 
 module M1
-  ( I.P1 p1[`V-1:0]
-  , output logic [`V-1:0]o_a
+  ( I.P1 p1[SIZE-1:0]
+  , output logic [SIZE-1:0]o_a
   , output logic en
   , output logic i_arst
   );
-  timeunit 1ns;
-  timeprecision 1ps;
   
-  for(genvar i =0; i< `V;i++) begin
+  for(genvar i =0; i< SIZE;i++) begin
+    always_comb p1[i].x = 1'b1;
     always_latch begin
       if (!i_arst)
         p1[i].y <= 1'b0;
       else if (en)
-        p1[i].y <= 1'b1;
+        p1[i].y <= p1[i].x;
     end
   assign o_a[i] = p1[i].y;
   end
 
 endmodule
 
-/* A different enable signal may be used in M2 to make en
- independent */ 
- 
 module M2
-  ( I.P2 p2[`V-1:0]
-  , output logic [`V-1:0]o_b
+  ( I.P2 p2[SIZE-1:0]
+  , output logic [SIZE-1:0]o_b
   , output logic en
   );
-  timeunit 1ns;
-  timeprecision 1ps;
 
-  for(genvar i =0; i< `V;i++) begin
+  for(genvar i =0; i< SIZE;i++) begin
     always_latch begin
      if (en)
-       o_b[i] <= p2[i].y;
+       p2[i].w <= p2[i].y;
     end
+  assign o_b[i] = p2[i].w;
   end
 
 endmodule
-
-
 
 module top
   ( input logic en
   , input logic i_clk
   , input logic i_arst
-  , output logic [`V-1:0]o_a 
-  , output logic [`V-1:0]o_b
+  , output logic [SIZE-1:0]o_a 
+  , output logic [SIZE-1:0]o_b
   );
-
-  timeunit 1ns;
-  timeprecision 1ps;
   
-  I u_I [`V-1:0] ();
+  I u_I [SIZE-1:0] ();
 
   M1 u_M1
     ( .p1(u_I)
