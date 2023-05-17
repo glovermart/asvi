2601922d542b0d1fc6c193aee1468710d5f99aa2
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-17 11:47:59 +0200

    changed text insertion from define to localparam

--- testcases/alwaysff_arrayofSVI_svi.sv
+++ testcases/alwaysff_arrayofSVI_svi.sv
@@ -1,7 +1,7 @@
 // Assignment via `always_ff` to scalar members within an SVI interface.
 // Array of SVIs
 
-`define duplicate 8
+localparam int SIZE = 8;
 
 interface I
   ( input logic i_clk
@@ -23,16 +23,16 @@ endinterface
 
 module top
   ( input logic i_clk
-  , output logic [`duplicate-1:0] o_a
-  , output logic [`duplicate-1:0] o_b
-  , output logic [`duplicate-1:0] o_c
+  , output logic [SIZE-1:0] o_a
+  , output logic [SIZE-1:0] o_b
+  , output logic [SIZE-1:0] o_c
   ); 
   timeunit 1ns;
   timeprecision 1ps;
   
-  I u_I [`duplicate-1:0] (.*);
+  I u_I [SIZE-1:0] (.*);
     
-  for (genvar i=0;i<`duplicate;i++)begin
+  for (genvar i=0;i<SIZE;i++)begin
     assign o_a[i] = u_I[i].x;      
     assign o_b[i] = u_I[i].y;  
     assign o_c[i] = u_I[i].z;    
