4a53fbe6e05e13b87fc1d4d4e02c1f12cf19ac6a
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-19 20:37:38 +0200

    initial work - tasks and functions

---
+++ testcases/void_function.sv
@@ -0,0 +1,19 @@
+// Function defintion within an interface.
+// Call function in combinatorial block.
+
+interface I;
+
+  function void adder (input int i_a, i_b, output int o_a); // Define function.
+    o_a = i_a + i_b;
+  endfunction
+endinterface
+
+module top
+  ( input int i_a
+  , input int i_b
+  , output int o_a
+  );
+
+  I u_I ();
+  always_comb u_I.adder(i_a,i_b,o_a); // Call function.
+endmodule
