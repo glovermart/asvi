d8e6fc94ccaf375e356db21ad04c368ac5f6fcf0
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-26 21:37:08 +0200

    Add comments and lines showing interesting or important stuff

--- testcases/alwaysff_misc_svi_hierachicalModuleRefInst.sv
+++ testcases/alwaysff_misc_svi_hierachicalModuleRefInst.sv
@@ -1,7 +1,9 @@
-//Assignment via `always_comb` to scalar members within an SVI.
+// Assignment via `always_ff` to scalar members within an SVI.
+// Hierachical referencing - module intance.
+// NOTE: Lines 29 to 31, 43 to 45, 65 to 67, and 88 to 90. 
 
 interface I
-  ( input var logic i_clk
+  ( input logic i_clk
   );
 
   logic z;
@@ -16,6 +18,7 @@ interface I
 
 endinterface
 
+
 module M1
   ( I u_I
   , output logic a
@@ -29,6 +32,7 @@ module M1
 
 endmodule
 
+
 module M2
   ( I u_I
   , output logic a
@@ -40,10 +44,12 @@ module M2
   assign b = u_M1.b;
   assign c = u_M1.c;
 
-
-  M1 u_M1 (.*);
+  M1 u_M1 
+    ( .*
+    );
 
 endmodule
+
 
 module M3
   ( I u_I
@@ -51,7 +57,10 @@ module M3
   , output logic b
   , output logic c
   );
-  M2 u_M2 (.*);
+  
+  M2 u_M2 
+    ( .*
+    );
 
   assign a = u_M2.a;
   assign b = u_M2.b;
@@ -59,17 +68,25 @@ module M3
 
 endmodule
 
+
 module top
-  ( input var logic i_clk
+  ( input logic i_clk
   , output logic a
   , output logic b
   , output logic c
   );
-  I u_I(.*);
+  
+  I u_I 
+    ( .*
+    );
 
-  M3 u_M3(.u_I(u_I),.*);
+  M3 u_M3 
+    ( .u_I  (u_I)
+    , .*
+    );
+  
   assign a = u_M3.a;
-  assign b = u_M3.b ;
+  assign b = u_M3.b;
   assign c = u_M3.c;
 
 endmodule
