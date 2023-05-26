760d8431def576b5c36603e2b6147a7ec4376615
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-26 21:15:23 +0200

    Add comments and lines showing interesting or important stuff

--- testcases/alwaysff_misc_port_signalPerModule_clocks.sv
+++ testcases/alwaysff_misc_port_signalPerModule_clocks.sv
@@ -1,4 +1,6 @@
-//Assignment via `always_ff` to scalar members of an SVI port.
+// Assignment via `always_ff` to scalar members of an SVI port.
+// NOTE: Lines 36, 50 to 54, and 68 to 72. Attempts to resolve signal 
+// driven by multiple clocks issue from testcase *singleModuleInstantiation.
 
 interface I
   ( output logic z
@@ -8,6 +10,7 @@ interface I
   , input logic i_srst
   , input logic i_arst
   );
+  
   logic w = 1'b1;
 
   modport P
@@ -22,53 +25,70 @@ interface I
 
 endinterface
 
+
 module M1
   ( I.P p
   );
+  
   localparam bit Z = 1'b0;
+  
   //No reset
   always_ff @(posedge p.i_clk) p.z <= Z;
 
 endmodule
 
+
 module M2
   ( I.P p
   );
-  M1 u_M1 (.p(p));
+
+  M1 u_M1 
+    ( .p  (p)
+    );
+  
   //Synchronous reset
   always_ff @(posedge p.i_clk)
-  if (p.i_srst)
+    if (p.i_srst)
       p.y <= 1'b0;
-  else
+    else
       p.y <= 1'b1;
 
 endmodule
 
+
 module M3
   ( I.P p
   );
-  M2 u_M2 (.p(p));
+  
+  M2 u_M2 
+    ( .p  (p)
+    );
+  
   //Asynchronous reset
   always_ff @(posedge p.i_clk, posedge p.i_arst)
-  if (p.i_arst)
-    p.x <= 1'b0;
-  else
-    p.x <= p.w;
+    if (p.i_arst)
+      p.x <= 1'b0;
+    else
+      p.x <= p.w;
+
 endmodule
 
+
 module top
-  ( input var logic i_clk
-  , input var logic i_srst
-  , input var logic i_arst
+  ( input logic i_clk
+  , input logic i_srst
+  , input logic i_arst
   , output logic z
   , output logic y
   , output logic x
   );
 
-  I u_I (.*);  //  Connect ports.
+  I u_I 
+    ( .* // Connect ports using implicit port connection style.
+    );  
   
   M3 u_M3
-    ( .p(u_I)
+    ( .p  (u_I)
     );
 
 endmodule
