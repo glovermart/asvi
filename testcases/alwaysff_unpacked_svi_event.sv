3913b9359fa2ec6649173c2e7092bc974bef9524
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-27 00:01:47 +0200

    Add comments and lines showing interesting or important stuff

---
+++ testcases/alwaysff_unpacked_svi_event.sv
@@ -0,0 +1,32 @@
+// Assignment via `always_ff` to an unpacked array within an interface.
+// Use `foreach` loop to iterate through array elements.
+// Always_ff with event control and non-blocking assignment.
+
+interface I
+  ( input logic i_clk
+  , input logic i_a
+  , output logic o_a [8] // SV allows C-like declaration of an unpacked array.
+  );
+
+  logic x [7:0]; // Unpacked array x with starting and ending addresses.
+  
+  always_ff @(posedge i_clk)
+    foreach (x[i])
+      x[i] <= i_a;
+   
+  assign o_a = x; // Copy array to output pins.
+
+endinterface
+
+
+module top
+  ( input logic i_clk
+  , input logic i_a
+  , output logic o_a [7:0]
+  );
+
+  I u_I 
+  ( .* // Implicit port connection (dot-star).
+  );
+  
+endmodule
