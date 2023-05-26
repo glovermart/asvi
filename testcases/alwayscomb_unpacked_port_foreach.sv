3644e7fcc0d06b481d3791071ccb7550eb4a261b
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-26 03:42:14 +0200

    Add comments and lines showing interesting or important stuff

--- testcases/alwayscomb_unpacked_port_foreach.sv
+++ testcases/alwayscomb_unpacked_port_foreach.sv
@@ -1,5 +1,6 @@
 // Assignment via `always_comb` and modport to an unpacked array of an interface.
 // Use `foreach` loop to iterate through array elements.
+// NOTE: Lines 17, 22, 23, and 36.
 
 interface I;
 
@@ -8,10 +9,12 @@ interface I;
   modport P
     ( output x
     );
+
 endinterface
 
+
 module M
-  (I.P p
+  ( I.P p
   , input logic i_a
   );
 
@@ -21,16 +24,19 @@ module M
 
 endmodule
 
+
 module top
   ( input logic i_a
   , output logic o_a [7:0]
   );
 
   I u_I ();
+
   M u_M
-    ( .p(u_I) // Connection without explicitly specify port view. i.e.`u_I.P`
-    , .i_a(i_a)
+    ( .p    (u_I) // Connection without explicitly specifying modport view again.
+    , .i_a  (i_a)
     );
+
   assign o_a = u_I.x; // Copy to output pins.
   
 endmodule
