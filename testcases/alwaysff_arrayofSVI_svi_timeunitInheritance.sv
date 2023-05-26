af9d5c12f8811009c79fd91d9bec7965010270aa
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-26 16:23:56 +0200

    Add comments and lines showing interesting or important stuff

--- testcases/alwaysff_arrayofSVI_svi_timeunitInheritance.sv
+++ testcases/alwaysff_arrayofSVI_svi_timeunitInheritance.sv
@@ -1,12 +1,14 @@
 // Assignment via `always_ff` to scalar members within an SVI interface.
-// Array of SVIs
+// Array of SVIs.
+// Interface timeunit inheritance from top module.
+// NOTE: Lines 12, 33, and 36.
 
 localparam int SIZE = 8;
+
 
 interface I
   ( input logic i_clk
   );
-  timeunit 1ns;
   timeprecision 1ps;
   
   logic x;
@@ -14,12 +16,13 @@ interface I
   logic z;
 
   always_ff @(posedge i_clk) begin
-      x <= 1'b1;
-      y <= 1'b0;
-      z <= 1'b1;
+    x <= 1'b1;
+    y <= 1'b0;
+    z <= 1'b1;
   end
 
 endinterface
+
 
 module top
   ( input logic i_clk
@@ -27,12 +30,14 @@ module top
   , output logic [SIZE-1:0] o_b
   , output logic [SIZE-1:0] o_c
   ); 
-  timeunit 1ns;
+  timeunit 1ns; // Interface I should inherit this timeunit. LRM 3.14.2.3
   timeprecision 1ps;
   
-  I u_I [SIZE-1:0] (.*);
+  I u_I [SIZE-1:0] 
+    ( .*
+    );
     
-  for (genvar i=0;i<SIZE;i++)begin
+  for (genvar i = 0; i < SIZE; i++) begin
     assign o_a[i] = u_I[i].x;      
     assign o_b[i] = u_I[i].y;  
     assign o_c[i] = u_I[i].z;    
