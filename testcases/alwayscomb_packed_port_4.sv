ad604992700500ae72c679cdd5aed0f7fd809612
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-26 02:45:48 +0200

    Add comments and lines showing interesting or important stuff

--- testcases/alwayscomb_packed_port_4.sv
+++ testcases/alwayscomb_packed_port_4.sv
@@ -1,22 +1,26 @@
 // Assignment via `always_comb` to a packed array of an interface port.
-// Use of modport expressions for slice of a 1D packed array.
-
+// Use of modport expressions for slices of a 1D packed array.
+// Two generic interfaces; two instances of same module.
+// NOTE: Lines 21, 22, 36, 37, 40, and 41. 
 
 interface I;
   
   logic [7:0] x;
+
   modport P1
     ( output .p1(x[3:0])
     );
+
   modport P2
     ( output .p2(x[7:4])
     );
 
 endinterface
 
+
 module M
-  ( interface i  // Generic interface.
-  , interface j
+  ( interface i  // Generic interface 1.
+  , interface j  // Generic interface 2.
   );            
 
   always_comb i.p1 = '0; //write 0000
@@ -24,11 +28,13 @@ module M
                         
 endmodule
 
+
 module top
   ( output logic [7:0] o_a
   );
 
   I u_I ();
+
   M u_M1 
     ( u_I.P1 //LSBs
     );
