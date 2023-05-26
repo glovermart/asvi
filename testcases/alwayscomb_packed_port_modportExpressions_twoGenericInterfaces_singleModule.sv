4e2cdb1f35dea0561f719d30ca37225016f9f74a
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-26 02:32:55 +0200

    Add comments and lines showing interesting or important stuff

--- testcases/alwayscomb_packed_port_modportExpressions_twoGenericInterfaces_singleModule.sv
+++ testcases/alwayscomb_packed_port_modportExpressions_twoGenericInterfaces_singleModule.sv
@@ -1,34 +1,40 @@
 // Assignment via `always_comb` to a packed array of an interface port.
-// Use of modport expressions for slice of a 1D packed array.
+// Use of modport expressions for slices of a 1D packed array.
 // A second generic interface in module `M`.
+// NOTE: Lines 22, 23, 26, 27, 39, and 40.
 
 interface I;
   
   logic [7:0] x;
+
   modport P1
     ( output .p1(x[3:0])
     );
+
   modport P2
     ( output .p2(x[7:4])
     );
 
 endinterface
 
+
 module M
-  ( interface i   //Generic interface
-  , interface j
+  ( interface i   // Generic interface 1.
+  , interface j   // Generic interface 2.
   );
 
   always_comb i.p1 = '0;
-  always_comb j.p2 = '1;// Generic interface `j` introduced to elimate questa error:
-                        // Failed to find 'p2' in hierarchical name 'i.p2'.
+  always_comb j.p2 = '1;
+                        
 endmodule
+
 
 module top
   ( output logic [7:0] o_a
   );
 
   I u_I ();
+
   M u_M 
     ( u_I.P1 //LSBs
     , u_I.P2 //MSBs
