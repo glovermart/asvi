6b454b2938061a5200a205d4e6f42facd3b8b1d2
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-29 01:15:37 +0200

    clean and rename

--- testcases/force_arrayofSVI_inst_gen_assign.sv
+++ testcases/force_arrayofSVI_inst_gen_assign.sv
@@ -1,12 +1,12 @@
 // Assignment via `force` to scalar members of an SVI instance.
 // Array of SVIs
-
-
 // Output a vector (SVI 1D array instance) of 0's,1's,z's
 // Copy to output pin via gen block and assign construct.
 
 interface I;
+  
   logic z;
+
 endinterface
 
 
@@ -16,20 +16,20 @@ module top
   , output logic [7:0] o_a
   );
 
- I u_I [7:0]();
+  I u_I [7:0]();
  
- for (genvar i = 0; i < 8; i++)
-   assign u_I[i].z = i_a;
+  for (genvar i = 0; i < 8; i++)
+    assign u_I[i].z = i_a;
 
- for (genvar i = 0; i < 8; i++) begin 
-   always_comb
-     if (en)
-       force u_I[i].z = 1'bz;
-     else
-       release u_I[i].z;
- end
+  for (genvar i = 0; i < 8; i++) begin 
+    always_comb
+      if (en)
+        force u_I[i].z = 1'bz;
+      else
+        release u_I[i].z;
+  end
 
- for (genvar i = 0; i < 8; i++)
-   assign o_a [i] = u_I[i].z;
+  for (genvar i = 0; i < 8; i++)
+    assign o_a [i] = u_I[i].z;
 
 endmodule
