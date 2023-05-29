93e51654cf523970ac369c39665088e35c60c7da
glovermart <63477001+glovermart@users.noreply.github.com>
2023-05-29 21:02:39 +0200

    clean and rename

--- testcases/task_export_modportHierachicalRef_protoype.sv
+++ testcases/task_export_modportHierachicalRef_protoype.sv
@@ -1,36 +1,43 @@
 // Task defined in a module; use of export to export task to interface.
 // Call task in combinational block.
 // adder method called in another module.
+// NOTE: Lines 9, 13, 23, and 37.
 
 interface I;
+  
   modport P1
-    ( import task adder (input int i_a, i_b, output int o_a)
+    ( import adder 
     );
+  
   modport P2
     ( export adder
     );
+
 endinterface
 
+
 module M1
-  ( interface j
+  ( I.P2 p2
   );
  
-  task j.adder (input int i_a, i_b, output int o_a); // Define task.
+  task p2.adder (input int i_a, i_b, output int o_a); // Hierrachical define.
     o_a = i_a + i_b;
   endtask
 
 endmodule
 
+
 module M2
-  ( interface i // Generic interface.
+  ( I.P1 p1
   , input int i_a
   , input int i_b
   , output int o_a
   );
 
-  always_comb i.adder(i_a,i_b,o_a); // Perform addition operation.
+  always_comb p1.adder(i_a,i_b,o_a); // Perform addition operation.
 
 endmodule
+
 
 module top
   ( input int i_a
@@ -39,13 +46,16 @@ module top
   );
 
   I u_I ();
+  
   M1 u_M1 
-    ( u_I.P2
+    ( .p2( u_I.P2)
     );
+  
   M2 u_M2
-    ( u_I.P1
-    , i_a
-    , i_b
-    , o_a
+    ( .p1(u_I.P1)
+    , .i_a
+    , .i_b
+    , .o_a
     );
+
 endmodule
--- testcases/task_export_modportNohierachicalRef.sv
+++ testcases/task_export_modportNohierachicalRef.sv
@@ -1,15 +1,20 @@
 // Task defined in a module; use of export to export task to interface.
 // Call task in combinational block.
 // adder method called in another module.
+// NOTE: Lines 9, 13, 23, and 37.
 
 interface I;
+  
   modport P1
     ( import adder 
     );
+
   modport P2
     ( export adder
     );
+
 endinterface
+
 
 module M1
   ( I.P2 p2
@@ -20,6 +25,7 @@ module M1
   endtask
 
 endmodule
+
 
 module M2
   ( I.P1 p1
@@ -32,6 +38,7 @@ module M2
 
 endmodule
 
+
 module top
   ( input int i_a
   , input int i_b
@@ -39,13 +46,16 @@ module top
   );
 
   I u_I ();
+  
   M1 u_M1 
-    ( .p2( u_I.P2)
+    ( .p2    (u_I.P2)
     );
-  M2 u_M2
-    ( .p1(u_I.P1)
+  
+    M2 u_M2
+    ( .p1    (u_I.P1)
     , .i_a
     , .i_b
     , .o_a
-    );
+    ); 
+
 endmodule
