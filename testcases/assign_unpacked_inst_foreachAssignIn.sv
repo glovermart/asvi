23933db312b854a3e4df6fd541b0b0cf235fa0f2
damc <dave.mcewan@nordicsemi.no>
2023-06-08 16:39:54 +0200

    walkthrough Loops `do`-`while` and `foreach` must be in a process.

--- testcases/assign_unpacked_inst_foreachAssignIn.sv
+++ testcases/assign_unpacked_inst_foreachAssignIn.sv
@@ -5,21 +5,21 @@
 interface I;
 
   logic x [7:0]; // Unpacked array x with start and end address.
-  
+
 endinterface
 
 
 module top
   ( input logic i_a
-  , output logic [7:0] o_a
+  , output logic [7:0] o_x
   );
 
   I u_I ();
-  
-    // Some tools output error messages during read step.
+
+  initial
     foreach (u_I.x[i])
-      assign u_I.x[i] = i_a; 
-  
-  assign o_a = u_I.x;
+      assign u_I.x[i] = i_a;
+
+  assign o_x = u_I.x;
 
 endmodule
